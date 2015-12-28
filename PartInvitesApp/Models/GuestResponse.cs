9876364656f2566f73a4a7533a9616b695ab4417
riad133 <riad.nazrul@gmail.com>
2015-12-29 01:30:56 +0600

    full upload

---
+++ PartInvitesApp/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartInvitesApp.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email.")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter valid email.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone no.")]
+        public string PhoneNo { get; set; }
+        [Required(ErrorMessage = "Are you attend?")]
+        public bool WillAttend { get; set; }
+    }
+}
